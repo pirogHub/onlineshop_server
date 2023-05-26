693b91ce520c454d9aa6f865b4bae95f6f29c4f0
My Name <1@2.3>
2023-05-26 17:01:43 +0400

    1701

--- src/main.ts
+++ src/main.ts
@@ -13,13 +13,13 @@ async function bootstrap() {
     resave: false,
     saveUninitialized: false
   }))
-
+  console.log("process.env.CLIENT_HOST", process.env.CLIENT_HOST);
   const configCors = {
     credentials: true,
     allowedHeaders: ['Origin', ' X-Requested-With', ' Content-Type', ' Accept', ' Authorization'],
     // origin: [process.env.CLIENT_HOST, "http://localhost:3000", "localhost:3000", "http://localhost:3001"],
-    // origin: process.env.CLIENT_HOST,
-    origin: "http://localhost:3000",
+    origin: process.env.CLIENT_HOST,
+    // origin: "http://localhost:3000",
     // origin: true,
     methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE", "OPTIONS"],
   }
@@ -35,7 +35,7 @@ async function bootstrap() {
   //   })
   // );
 
-  console.log("process.env.CLIENT_HOST", process.env.CLIENT_HOST);
+
   console.log("configCors", configCors);
   const config = new DocumentBuilder()
     .setTitle('Online Shop')
